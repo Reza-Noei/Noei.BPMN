b69d1c48516bed0cae5e1a996adaa93a1139b517
Reza Noei <Reza.Noei@chmail.ir>
2024-10-22 01:27:51 +0330

    Add Angular front-end.

--- Noei.BPMS/Noei.BPMS.AppHost/Program.cs
+++ Noei.BPMS/Noei.BPMS.AppHost/Program.cs
@@ -1,7 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.Noei_BPMS_Service>("noei-bpms-service");
-
 builder.AddProject<Projects.Noei_BPMS_DatabaseMigration>("noei-bpms-databasemigration");
+
+builder.AddProject<Projects.Noei_BPMS_Web_Server>("noei-bpms-web-server");
 
 builder.Build().Run();
